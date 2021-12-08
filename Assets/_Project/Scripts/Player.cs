955046512d4a2f6109bb66f5f6708913e7a0be99
Matt Stockton <matt.stockton17@gmail.com>
2021-12-08 14:52:10 -0800

    Added speed variable to player.

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] float _speed = 5f;
 
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
     }
