a9d44bb3b5726803754ba6f491560f153fc6fa3b
Matt Stockton <matt.stockton17@gmail.com>
2021-12-09 13:58:39 -0800

    Added direction and speed variables to fly.

--- Assets/_Project/Scripts/Fly.cs
+++ Assets/_Project/Scripts/Fly.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
-    Vector3 _startPosition;
-    Vector2 _direction = Vector2.up;
+    Vector2 _startPosition;
+    [SerializeField] Vector2 _direction = Vector2.up;
+    [SerializeField] float _maxDistance = 2f;
+    [SerializeField] float _speed = 3f;
 
     void Start()
     {
@@ -12,10 +14,11 @@ public class Fly : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(_direction * Time.deltaTime);
-        var distance = Vector2.distance(_startPosition, transform.position);
-        if (distance >= 2f)
+        transform.Translate(_direction.normalized * Time.deltaTime * _speed);
+        var distance = Vector2.Distance(_startPosition, transform.position);
+        if (distance >= _maxDistance)
         {
+            transform.position = _startPosition + (_direction.normalized * _maxDistance);
             _direction *= -1;
         }
     }
