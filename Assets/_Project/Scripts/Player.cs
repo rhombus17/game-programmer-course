405dea56000595f7be01f860084c01cde36bbe04
Matt Stockton <matt.stockton17@gmail.com>
2022-01-13 14:00:48 -0800

    Refactored and cached player class.

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -11,56 +11,44 @@ public class Player : MonoBehaviour
     [SerializeField] float _downPull = 2f;
     [SerializeField] float _maxJumpDuration = 5f;
 
+    Rigidbody2D _rigidbody2D;
+    Animator _animator;
+    SpriteRenderer _spriteRenderer;
     Vector2 _startPosition;
+    bool _isGrounded;
     int _jumpsRemaining;
     float _fallTimer;
     float _jumpTimer = 0f;
+    float _horizontal;
 
     void Start()
     {
         _startPosition = transform.position;
         _jumpsRemaining = _maxJumps;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default"));
-        bool isGrounded = hit != null;
+        UpdateIsGrounded();
+        ReadHorizontalInput();
+        MoveHorizontal();
 
-        var horizontal = Input.GetAxis("Horizontal") * _speed;
-        var rigidbody2D = GetComponent<Rigidbody2D>();
 
-        if (Mathf.Abs(horizontal) >= 1)
-        {
-            rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
-        }
+        UpdateAnimator();
+        UpdateSpriteDirection();
 
-        var animator = GetComponent<Animator>();
-        bool walking = horizontal != 0;
-        animator.SetBool("Walk", walking);
 
-        if (horizontal != 0)
-        {
-            var spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.flipX = horizontal < 0;
-        }
-
-        if ((Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space)) && _jumpsRemaining > 0)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
-            _jumpsRemaining--;
-            _jumpTimer = 0f;
-            _fallTimer = 0f;
-        }
-        else if ((Input.GetButton("Fire1") || Input.GetKey(KeyCode.Space)) && _jumpTimer <= _maxJumpDuration)
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
-            _fallTimer = 0f;
-        }
+        if (ShouldStartJump())
+            Jump();
+        else if (ShouldContinueJump())
+            ContinueJump();
 
         _jumpTimer += Time.deltaTime;
-        
-        if (isGrounded && _fallTimer > 0)
+
+        if (_isGrounded && _fallTimer > 0)
         {
             _jumpsRemaining = _maxJumps;
             _fallTimer = 0f;
@@ -69,8 +57,65 @@ public class Player : MonoBehaviour
         {
             _fallTimer += Time.deltaTime;
             var downForce = _downPull * _fallTimer * _fallTimer;
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y - downForce);
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y - downForce);
         }
+    }
+
+    void UpdateIsGrounded()
+    {
+        var hit = Physics2D.OverlapCircle(_feet.position, 0.1f, LayerMask.GetMask("Default"));
+        _isGrounded = hit != null;
+    }
+
+    void ReadHorizontalInput()
+    {
+        _horizontal = Input.GetAxis("Horizontal") * _speed;
+    }
+
+    void MoveHorizontal()
+    {
+        if (Mathf.Abs(_horizontal) >= 1)
+        {
+            _rigidbody2D.velocity = new Vector2(_horizontal, _rigidbody2D.velocity.y);
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        bool walking = _horizontal != 0;
+        _animator.SetBool("Walk", walking);
+    }
+
+    void UpdateSpriteDirection()
+    {
+        if (_horizontal != 0)
+        {
+            _spriteRenderer.flipX = _horizontal < 0;
+        }
+    }
+
+    bool ShouldStartJump()
+    {
+        return (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space)) && _jumpsRemaining > 0;
+    }
+
+    bool ShouldContinueJump()
+    {
+        return (Input.GetButton("Fire1") || Input.GetKey(KeyCode.Space)) && _jumpTimer <= _maxJumpDuration;
+    }
+
+    void Jump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpVelocity);
+        _jumpsRemaining--;
+        _jumpTimer = 0f;
+        _fallTimer = 0f;
+    }
+
+    void ContinueJump()
+    {
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpVelocity);
+        _fallTimer = 0f;
     }
 
     public void ResetToStart()
