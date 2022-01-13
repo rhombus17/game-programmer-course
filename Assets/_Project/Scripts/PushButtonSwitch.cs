06eae4e6e1eef5d09ad59569cab3759171981148
Matt Stockton <matt.stockton17@gmail.com>
2022-01-13 14:59:31 -0800

    Made button release when player leaves collider.

--- Assets/_Project/Scripts/PushButtonSwitch.cs
+++ Assets/_Project/Scripts/PushButtonSwitch.cs
@@ -1,12 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class PushButtonSwitch : MonoBehaviour
 {
-    [SerializeField] Sprite _downSprite;
-    [SerializeField] UnityEvent _onEnter;
+    [SerializeField] Sprite _depressedSprite;
+    [SerializeField] UnityEvent _onPressed;
+    [SerializeField] UnityEvent _onReleased;
+
+    SpriteRenderer _spriteRenderer;
+    Sprite _releasedSprite;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _releasedSprite = _spriteRenderer.sprite;
+        BecomeReleased();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,8 +23,27 @@ public class PushButtonSwitch : MonoBehaviour
         if (player == null)
             return;
 
-        var spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = _downSprite;
-        _onEnter?.Invoke();
+        BecomePressed();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        var player = other.GetComponent<Player>();
+        if (player == null)
+            return;
+        
+        BecomeReleased();
+    }
+
+    void BecomePressed()
+    {
+        _spriteRenderer.sprite = _depressedSprite;
+        _onPressed?.Invoke();
+    }
+
+    void BecomeReleased()
+    {
+        _spriteRenderer.sprite = _releasedSprite;
+        _onReleased?.Invoke();
     }
 }
