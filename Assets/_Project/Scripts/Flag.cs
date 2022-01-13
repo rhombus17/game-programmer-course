94b112d2a7619238be3fb30d8a594c25c5a53597
Matt Stockton <matt.stockton17@gmail.com>
2022-01-13 11:54:46 -0800

    Added level progression.

--- Assets/_Project/Scripts/Flag.cs
+++ Assets/_Project/Scripts/Flag.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Flag : MonoBehaviour
 {
+    [SerializeField] string _sceneName;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
@@ -13,6 +16,8 @@ public class Flag : MonoBehaviour
         // Play flag wave
         var animator = GetComponent<Animator>();
         animator.SetTrigger("Raise");
+
         // Load new level
+        SceneManager.LoadScene(_sceneName);
     }
 }
