def7e30a2d2374d39211ca99c7836e277d0f359a
Matt Stockton <matt.stockton17@gmail.com>
2022-01-20 09:35:17 -0800

    Added turn around logic to slime.

--- Assets/_Project/Scripts/Slime.cs
+++ Assets/_Project/Scripts/Slime.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform _sensorLeft;
+    [SerializeField] Transform _sensorRight;
     Rigidbody2D _rigidbody2d;
+    float _direction = -1f;
 
     void Awake()
     {
@@ -13,7 +16,23 @@ public class Slime : MonoBehaviour
 
     void Update()
     {
-        _rigidbody2d.velocity = Vector2.left;
+        var desiredVelocity = new Vector2(-1, _rigidbody2d.velocity.y);
+        _rigidbody2d.velocity = desiredVelocity;
+        Transform sensor = _direction > 0 ? _sensorLeft : _sensorLeft;
+
+        Debug.DrawRay(sensor.position, Vector2.down * 0.1f, Color.red);
+        var result = Physics2D.Raycast(sensor.position, Vector2.down, 0.1f);
+        if (result.collider == null)
+        {
+            TurnAround();
+        }
+    }
+
+    void TurnAround()
+    {
+        _direction *= -1;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = _direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D other)
