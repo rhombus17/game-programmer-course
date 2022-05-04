10fe51216faf6be16ff3ec367f807917e190fac0
Matt Stockton <matt.stockton17@gmail.com>
2022-05-04 15:43:31 -0700

    Completed falling platform and added options and attributes.

--- Assets/_Project/Scripts/FallingPlatform.cs
+++ Assets/_Project/Scripts/FallingPlatform.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    [SerializeField] float _wiggleStrength;
+    [SerializeField] float _shakeX;
+    [SerializeField] float _shakeY;
+    [Tooltip("Resets the shake timer when no players are on the platform")]
+    [SerializeField] bool _resetShakeTimerOnEmpty;
+    [Range(0.1f, 5f)] [SerializeField] float _fallAfterSeconds;
+    [SerializeField] float _fallSpeed;
     bool _playerInside;
+    bool _isFalling = false;
     HashSet<Player> _playersInTrigger = new HashSet<Player>();
     Coroutine _coroutine;
     Transform _transform;
     Vector3 _startPostion;
+    float _wiggleTimer;
 
     void Start()
     {
         _transform = transform;
         _startPostion = _transform.position;
+        _isFalling = false;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -35,28 +43,46 @@ public class FallingPlatform : MonoBehaviour
     {
         // Wait to wiggle
         yield return new WaitForSeconds(0.25f);
+
         // Wiggle
-        float wiggleTimer = 0f;
-        while (wiggleTimer < 1f)
+        // float wiggleTimer = 0f;
+        while (_wiggleTimer < _fallAfterSeconds)
         {
-            float min = _wiggleStrength / 100f * -1f;
-            float max = _wiggleStrength / 100f;
-            float randX = UnityEngine.Random.Range(min, max);
-            float randY = UnityEngine.Random.Range(min, max);
+            // float min = _wiggleStrength / 100f * -1f;
+            // float max = _wiggleStrength / 100f;
+            float randX = UnityEngine.Random.Range(-_shakeX / 100f, _shakeX / 100f);
+            float randY = UnityEngine.Random.Range(-_shakeY / 100f, _shakeY / 100f);
             _transform.position = _startPostion + new Vector3(randX, randY);
             float randDelay = UnityEngine.Random.Range(0.005f, 0.01f);
             yield return new WaitForSeconds(randDelay);
-            wiggleTimer += randDelay;
+            _wiggleTimer += randDelay;
         }
-        // yield return new WaitForSeconds(1f);
+
         // Fall
-        Debug.Log($"Falling");
-        yield return new WaitForSeconds(3f);
+        _isFalling = true;
+        var colliders = GetComponents<Collider2D>();
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        float fallTimer = 0f;
+        while (fallTimer < 3f)
+        {
+            _transform.position += Vector3.down * Time.deltaTime * _fallSpeed;
+            fallTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
         
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        if (_isFalling)
+            return;
+
         var player = other.GetComponent<Player>();
         if (player == null)
             return;
@@ -67,6 +93,11 @@ public class FallingPlatform : MonoBehaviour
         {
             _playerInside = false;
             StopCoroutine(_coroutine);
+
+            if (_resetShakeTimerOnEmpty)
+            {
+                _wiggleTimer = 0f;
+            }
         }
     }
 }
