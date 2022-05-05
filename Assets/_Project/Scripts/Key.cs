483e0368a4f6748afb16b706b426e5d4d984f4ee
Matt Stockton <matt.stockton17@gmail.com>
2022-05-05 11:48:12 -0700

    Added key and lock. Made falling platform reset position on exit.

---
+++ Assets/_Project/Scripts/Key.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] KeyLock _keyLock;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        bool playerCollision = CheckPlayerCollision(other);
+        if (playerCollision)
+            return;
+
+        bool lockCollision = CheckLockCollision(other);
+        if (lockCollision)
+            return;
+    }
+
+    bool CheckPlayerCollision(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player == null)
+            return false;
+
+        transform.SetParent(player.transform);
+        transform.localPosition = Vector3.up;
+        return true;
+    }
+
+    bool CheckLockCollision(Collider2D other)
+    {
+        if (_keyLock == null)
+            return false;
+        
+        KeyLock keyLock = other.GetComponent<KeyLock>();
+        if (keyLock == null || keyLock != _keyLock)
+            return false;
+
+        keyLock.Unlock();
+        Destroy(gameObject);
+        return true;
+    }
+}
