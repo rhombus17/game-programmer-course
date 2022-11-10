ff50354ac89a3803889199edfc8eded0be5398de
Matt Stockton <matt.stockton17@gmail.com>
2022-11-10 15:30:57 -0800

    Added animations for box hits.

--- Assets/_Project/Scripts/HittableFromBelow.cs
+++ Assets/_Project/Scripts/HittableFromBelow.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 
 public class HittableFromBelow : MonoBehaviour
 {
     [SerializeField] protected Sprite _emptyBoxSprite;
+    Animator _animator;
+    
+    static readonly int UseHashID = Animator.StringToHash("Use");
 
     protected virtual bool CanUse => true;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -18,9 +27,16 @@ public class HittableFromBelow : MonoBehaviour
         if (col.contacts[0].normal.y <= 0)
             return;
 
+        PlayAnimation();
         Use();
         if (!CanUse)
             GetComponent<SpriteRenderer>().sprite = _emptyBoxSprite;
+    }
+
+    void PlayAnimation()
+    {
+        if (_animator != null)
+            _animator.SetTrigger(UseHashID);
     }
 
     protected virtual void Use()
