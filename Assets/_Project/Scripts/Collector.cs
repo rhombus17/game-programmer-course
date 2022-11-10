65aea255ee69c4ef556ffa390787ae7b96f20723
Matt Stockton <matt.stockton17@gmail.com>
2022-11-09 16:03:12 -0800

    Updated collector to show a count.

--- Assets/_Project/Scripts/Collector.cs
+++ Assets/_Project/Scripts/Collector.cs
@@ -1,10 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
-    
-    
+
+    TMP_Text _remainingCollectiblesText;
+    int _curCount;
+
+    void Awake()
+    {
+        _remainingCollectiblesText = GetComponentInChildren<TMP_Text>();
+        UpdateCount(_collectibles.Length);
+    }
+
+    void Update()
+    {
+        var newCount = 0;
+        foreach (var collectible in _collectibles)
+        {
+            if (collectible.isActiveAndEnabled)
+                newCount++;
+        }
+
+        if (newCount != _curCount)
+            UpdateCount(newCount);
+
+        if (newCount > 0)
+            return;
+        
+        // All Gems Collected
+    }
+
+    void UpdateCount(int newCount)
+    {
+        _curCount = newCount;
+        _remainingCollectiblesText?.SetText(newCount.ToString());
+    }
 }
